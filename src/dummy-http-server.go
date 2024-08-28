9173f6487e93685e075e1c2d44aed598ebb294c8
tedezed <jmtorres@cartodb.com>
2024-08-28 10:11:33 +0200

    Add log

--- src/dummy-http-server.go
+++ src/dummy-http-server.go
@@ -14,13 +14,30 @@ var host string
 var port string
 var config string
 
+func ReadUserIP(req *http.Request) string {
+    IPAddress := req.Header.Get("X-Real-Ip")
+    if IPAddress == "" {
+        IPAddress = req.Header.Get("X-Forwarded-For")
+    }
+    if IPAddress == "" {
+        IPAddress = req.RemoteAddr
+    }
+    return IPAddress
+}
+
+func LogRequest(req *http.Request) {
+    fmt.Println("[INFO] " + req.Method + " "  + req.URL.Path + " - IP: " + ReadUserIP(req))
+}
+
 func hello(w http.ResponseWriter, req *http.Request) {
 
+    LogRequest(req)
     fmt.Fprintf(w, "dummy server\n")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
 
+    LogRequest(req)
     for name, headers := range req.Header {
         for _, h := range headers {
             fmt.Fprintf(w, "%v: %v\n", name, h)
